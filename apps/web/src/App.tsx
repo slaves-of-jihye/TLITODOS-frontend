e296825f8989e1745d7fbea16d204d360a07c733
ketarubot <robin09v@naver.com>
2026-07-16 09:11:49 +0900

    feat: implement tlitodos product screens

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,5 +1,22 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
+import { useSessionStore } from "./app/sessionStore";
+import { LoginModal } from "./components";
+import { AlarmPage, DiaryPage, FriendHome, GroupHome, MyHome, NotFoundPage, ProfilePage } from "./screens";
+
 function App() {
-  return <>테스트~~</>;
+  const accessToken = useSessionStore((state) => state.accessToken);
+  return <HashRouter>
+    {accessToken ? <Routes>
+      <Route path="/" element={<MyHome/>}/>
+      <Route path="/alarm" element={<AlarmPage/>}/>
+      <Route path="/profile" element={<ProfilePage/>}/>
+      <Route path="/diary" element={<DiaryPage/>}/>
+      <Route path="/groups/:groupId" element={<GroupHome/>}/>
+      <Route path="/groups/:groupId/members/:userId" element={<FriendHome/>}/>
+      <Route path="*" element={<NotFoundPage/>}/>
+    </Routes> : null}
+    <LoginModal/>
+  </HashRouter>;
 }
 
 export default App;
